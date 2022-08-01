5962b61bae9d99956db5a79aadfe353ca26b066b
Joonas Harjumäki <jharjuma@gmail.com>
2022-08-01 08:45:00 +0300

    Reformat

--- sanulimestari/__init__.py
+++ sanulimestari/__init__.py
@@ -1,27 +1,28 @@
 import xml.etree.ElementTree as ET
 
-def parse_words(document, word_length, letters):
-	words = []
-	root = ET.fromstring(document)
-	for word in root.findall('.//st/s'):
-		if len(word.text) == word_length:
-			normalized_word = word.text.lower()
-			if not set(normalized_word) - letters:
-				words.append(normalized_word)
 
-	return words
+def parse_words(document, word_length, letters):
+    words = []
+    root = ET.fromstring(document)
+    for word in root.findall('.//st/s'):
+        if len(word.text) == word_length:
+            normalized_word = word.text.lower()
+            if not set(normalized_word) - letters:
+                words.append(normalized_word)
+
+    return words
 
 
 def guess_word(guess, word):
 
-	if len(guess) != len(word):
-		raise ValueError("The 'word' and the 'guess' must have same length!")
+    if len(guess) != len(word):
+        raise ValueError("The 'word' and the 'guess' must have same length!")
 
-	match = []
-	for i in range(0, len(guess)):
-		if guess[i] == word[i]:
-			match.append('+')
-		else:
-			match.append('-')
+    match = []
+    for i in range(0, len(guess)):
+        if guess[i] == word[i]:
+            match.append('+')
+        else:
+            match.append('-')
 
-	return (''.join(match), [])
+    return (''.join(match), [])
--- test/test_sanulimestari.py
+++ test/test_sanulimestari.py
@@ -17,8 +17,9 @@ def test_parse_words():
     <st><s>pito</s><t><tn>40</tn></t></st>
     </kotus-sanalista>
     """
-    letters = {'m', 'i', 'k', 'a','s', 'd', 'e' }
-    assert (['mika', 'sade'])  == parse_words(doc, 4, letters)
+    letters = {'m', 'i', 'k', 'a', 's', 'd', 'e'}
+    assert (['mika', 'sade']) == parse_words(doc, 4, letters)
+
 
 @pytest.mark.parametrize('guess,word,expected', [('b', 'a', ('-', [])),
                                                  ('a', 'a', ('+', [])),
@@ -30,6 +31,9 @@ def test_parse_words():
 def test_guess_word(guess, word, expected):
     assert expected == guess_word(guess, word)
 
+
 def test_guess_word_lenghts_dont_match():
-    with pytest.raises(ValueError, match="The 'word' and the 'guess' must have same length!"):
+    with pytest.raises(
+            ValueError,
+            match="The 'word' and the 'guess' must have same length!"):
         guess_word('a', 'ab')
